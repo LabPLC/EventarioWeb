b2e45902892db610f10aed573d1748136ca51813
Ryan Closner <ryan@ryanclosner.com>
2014-05-20 10:51:45 -0500

    Add engine light status endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Bootstrap::Application.routes.draw do
 
   devise_for :users
+
   resources :eventos
+
   get '/mapa'=> 'eventos#mapa', id: 'mapa'
   get '/about'    => 'high_voltage/pages#show', id: 'about'
   get '/contact'  => 'high_voltage/pages#show', id: 'contact'
@@ -9,6 +11,9 @@ Bootstrap::Application.routes.draw do
   get '/terms'    => 'high_voltage/pages#show', id: 'terms'
   get '/hola'    => 'high_voltage/pages#show', id: 'hola'
   get '/eventos_new'    => 'high_voltage/eventos#show', id: 'new'
+
+  get "/.well-known/status" => "status#check"
+
   root :to => 'high_voltage/pages#show', id: 'home'
 
 end
